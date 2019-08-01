85e4929ed8f701d72402613f0a097361fb4ee589
Pablo <solorzanopablo81@gmail.com>
2019-08-01 02:00:25 -0500

    Listar Productos y sus imagenes destacadas
    Mostrar la información con imagenes desde una url cargada en la base de datos

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,14 +2,17 @@
 
 namespace App\Http\Controllers;
 
+
 use App\Product;
 use Illuminate\Http\Request;
 
 class TestController extends Controller
 {
 	public function welcome(){
-		$productos=Product::all();
-		return view('welcome')->with(compact('productos'));
+		$productos = Product::all();
+    	return view('welcome')->with(compact('productos'));
+		/*$productos=Product::all();
+		return view('welcome')->with(compact('productos'));*/
 	}
 	
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,17 @@
-<?php@extends('layouts.app')
+@extends('layouts.app')
 
-namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
+
 @section('body-class','landing-page sidebar-collapse')
 @section('content')
    <div class="page-header header-filter" data-parallax="true" style="background-image: url('{{asset('img/profile_city.jpg')}}')">
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <h1 class="title">Your Story Starts With Us.</h1>
-          <h4>Every landing page needs a small description after the big bold title, that&apos;s why we added this text here. Add here all the information that can make you or your product create the first impression.</h4>
+          <h1 class="title">Bienvenidos A Su tienda Online</h1>
+          <h4>Realiza pedidos en línea y te contactaremos para coordinar la entrega</h4>
           <br>
           <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank" class="btn btn-danger btn-raised btn-lg">
-            <i class="fa fa-play"></i> Watch video
+            <i class="fa fa-play"></i> ¿Como funciona?
           </a>
         </div>
       </div>
@@ -22,8 +22,8 @@ namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
       <div class="section text-center">
         <div class="row">
           <div class="col-md-8 ml-auto mr-auto">
-            <h2 class="title">Let&apos;s talk product</h2>
-            <h5 class="description">This is the paragraph where you can write more details about your product. Keep you user engaged by providing meaningful information. Remember that by this time, the user is curious, otherwise he wouldn&apos;t scroll to get here. Add a button if you want the user to see more.</h5>
+            <h2 class="title">¿Por qué App Shop?</h2>
+            <h5 class="description">Puedes Revisar nuestra relación completa de productos, comparar precios y realizar tus pedidos cuando estes seguro</h5>
           </div>
         </div>
         <div class="features">
@@ -33,8 +33,8 @@ namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
                 <div class="icon icon-info">
                   <i class="material-icons">chat</i>
                 </div>
-                <h4 class="info-title">Free Chat</h4>
-                <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                <h4 class="info-title">Atendemos tus dudas</h4>
+                <p>Atendemos rápidamente cualquier consulta que tengas vía chat. No estás sólo, sino que siempre estamos atentos a tus inquietudes.</p>
               </div>
             </div>
             <div class="col-md-4">
@@ -42,8 +42,8 @@ namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
                 <div class="icon icon-success">
                   <i class="material-icons">verified_user</i>
                 </div>
-                <h4 class="info-title">Verified Users</h4>
-                <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                <h4 class="info-title">Pago Seguro</h4>
+                <p>Todo pedido que realices será confirmado a través de una llamada. Si no confias en los pagos en línea puedes pagar contra entrega el valor acordado</p>
               </div>
             </div>
             <div class="col-md-4">
@@ -51,8 +51,8 @@ namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
                 <div class="icon icon-danger">
                   <i class="material-icons">fingerprint</i>
                 </div>
-                <h4 class="info-title">Fingerprint</h4>
-                <p>Divide details about your product or agency work into parts. Write a few lines about each one. A paragraph describing a feature will be enough.</p>
+                <h4 class="info-title">Información privada</h4>
+                <p>Los pedidos que realices sólo los conocerás tú a través de tu panel de usuario. Nadie más tiene acceso a esta información</p>
               </div>
             </div>
           </div>
@@ -67,11 +67,11 @@ namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
               <div class="team-player">
                 <div class="card card-plain">
                   <div class="col-md-6 ml-auto mr-auto">
-                    <img src="{{asset('img/faces/avatar.jpg')}}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
+                    <img src="{{$producto->images()->first()->image}}" alt="Thumbnail Image" class="img-raised rounded-circle img-fluid">
                   </div>
                   <h4 class="card-title">{{$producto->name}}
                     <br>
-                    <small class="card-description text-muted">zWFZNMVXVDEV</small>
+                    <small class="card-description text-muted">{{$producto->category->name}}</small>
                   </h4>
                   <div class="card-body">
                     <p class="card-description">{{$producto->description}}</p>
@@ -91,31 +91,31 @@ namespace C:\xampp\htdocs\proyectos\app-shop\appShop\resources\views;
       <div class="section section-contacts">
         <div class="row">
           <div class="col-md-8 ml-auto mr-auto">
-            <h2 class="text-center title">Work with us</h2>
-            <h4 class="text-center description">Divide details about your product or agency work into parts. Write a few lines about each one and contact us about any further collaboration. We will responde get back to you in a couple of hours.</h4>
+            <h2 class="text-center title">¿Aún no te has registrado</h2>
+            <h4 class="text-center description">Regístrate ingresando tus datos básicos, y podrás realizar tus pedidos a través de nuestro carrito de compras. Si aún no te decides, de todas formas, con tu cuenta de usuario podrás hacer todas tus consultas sin compromiso</h4>
             <form class="contact-form">
               <div class="row">
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label class="bmd-label-floating">Your Name</label>
+                    <label class="bmd-label-floating">Nombre</label>
                     <input type="email" class="form-control">
                   </div>
                 </div>
                 <div class="col-md-6">
                   <div class="form-group">
-                    <label class="bmd-label-floating">Your Email</label>
+                    <label class="bmd-label-floating">Correo electrónico</label>
                     <input type="email" class="form-control">
                   </div>
                 </div>
               </div>
               <div class="form-group">
-                <label for="exampleMessage" class="bmd-label-floating">Your Message</label>
+                <label for="exampleMessage" class="bmd-label-floating">Tu mensaje</label>
                 <textarea type="email" class="form-control" rows="4" id="exampleMessage"></textarea>
               </div>
               <div class="row">
                 <div class="col-md-4 ml-auto mr-auto text-center">
                   <button class="btn btn-primary btn-raised">
-                    Send Message
+                    Enviar Consulta
                   </button>
                 </div>
               </div>
