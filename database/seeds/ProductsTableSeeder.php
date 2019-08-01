31b16de67ce28a6af7bd926c0e8770f706b8e30d
Pablo <solorzanopablo81@gmail.com>
2019-08-01 01:09:12 -0500

    Factory con relaciones

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -14,8 +14,24 @@ class ProductsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(Category::class, 5)->create();
+        /*factory(Category::class, 5)->create();
         factory(Product::class, 100)->create();
-        factory(ProductImage::class, 200)->create();
+        factory(ProductImage::class, 200)->create();*/
+        $categories=factory(Category::class, 5)->create();
+        $categories->each(function ($category) {
+            $productos=factory(Product::class, 20)->make();
+            $category->products()->saveMany($productos);
+            //se usa save() cuando se pone una instancia de Model y en esta parte pasamos una coleccion
+            $productos->each(function ($p) {
+                $images=factory(ProductImage::class, 5)->make();
+                $p->images()->saveMany($images);
+            });
+          });  
+
+        /*$users = factory(App\User::class, 3)
+           ->create()
+           ->each(function ($user) {
+                $user->posts()->save(factory(App\Post::class)->make());
+            });*/
     }
 }
