4497f040669bfadf36bc4dc9d4a361379f980237
Pablo <solorzanopablo81@gmail.com>
2019-08-03 17:41:55 -0500

    Incluir footer como vista hija

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,72 +20,73 @@
 </head>
 
 <body class="@yield('body-class')">
-  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
-    <div class="container">
-      <div class="navbar-translate">
-        <a class="navbar-brand" href="{{url('/')}}">
-        App Shop </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-          <span class="navbar-toggler-icon"></span>
-      </button>
-  </div>
-  <div class="collapse navbar-collapse">
-    <ul class="navbar-nav ml-auto">
-      @guest
-      <li class="nav-item">
-        <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
-    </li>
-    @if (Route::has('register'))
-    <li class="nav-item">
-        <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
-    </li>
-    @endif
-    @else
-    <li class="nav-item dropdown">
-        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-            {{ Auth::user()->name }} <span class="caret"></span>
-        </a>
-
-        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-          @if(auth()->user()->admin)
-            <a class="dropdown-item" href="{{ url('admin/productos') }}">Gestionar Productos</a>
+    <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+          <div class="container">
+            <div class="navbar-translate">
+              <a class="navbar-brand" href="{{url('/')}}">
+              App Shop </a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse">
+          <ul class="navbar-nav ml-auto">
+            @guest
+            <li class="nav-item">
+              <a class="nav-link" href="{{ route('login') }}">{{ __('Ingresar') }}</a>
+          </li>
+          @if (Route::has('register'))
+          <li class="nav-item">
+              <a class="nav-link" href="{{ route('register') }}">{{ __('Registro') }}</a>
+          </li>
           @endif
-            <a class="dropdown-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-            document.getElementById('logout-form').submit();">
-            {{ __('Desconectarse') }}
-        </a>
+          @else
+          <li class="nav-item dropdown">
+              <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                  {{ Auth::user()->name }} <span class="caret"></span>
+              </a>
 
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-    </div>
-</li>
-@endguest
-<li class="nav-item">
-    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" data-original-title="Follow us on Twitter">
-      <i class="fa fa-twitter"></i>
-  </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank" data-original-title="Like us on Facebook">
-      <i class="fa fa-facebook-square"></i>
-  </a>
-</li>
-<li class="nav-item">
-    <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank" data-original-title="Follow us on Instagram">
-      <i class="fa fa-instagram"></i>
-  </a>
-</li>
-</ul>
-</div>
-</div>
+              <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                @if(auth()->user()->admin)
+                  <a class="dropdown-item" href="{{ url('admin/productos') }}">Gestionar Productos</a>
+                @endif
+                  <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+                  {{ __('Desconectarse') }}
+              </a>
+
+              <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                  @csrf
+              </form>
+          </div>
+      </li>
+      @endguest
+      <li class="nav-item">
+          <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank" data-original-title="Follow us on Twitter">
+            <i class="fa fa-twitter"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank" data-original-title="Like us on Facebook">
+            <i class="fa fa-facebook-square"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+          <a class="nav-link" rel="tooltip" title="" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank" data-original-title="Follow us on Instagram">
+            <i class="fa fa-instagram"></i>
+        </a>
+      </li>
+      </ul>
+      </div>
+      </div>
 </nav>
 <div class="wrapper">
     @yield('content')
+    @include('includes.footer')
 </div>
 
 <script src="{{asset('/js/core/jquery.min.js')}}" type="text/javascript"></script>
